9e113a0097e2d7b263b1228fa80662f2fa2d7c7b
Jørgen Sirhaug <sirhaug751@gmail.com>
2015-10-22 11:07:02 +0200

    'posts' table modified to show if post is answered by a doctor

--- securitywebapp/src/webapp/Sql.php
+++ securitywebapp/src/webapp/Sql.php
@@ -19,7 +19,7 @@ class Sql
     static function up()
     {
         $q1 = "CREATE TABLE users (id INTEGER PRIMARY KEY, user VARCHAR(50), pass VARCHAR(255), email varchar(50) default null, fullname varchar(50), address varchar(50), postcode varchar (4), age varchar(50), bio varhar(50), isadmin INTEGER);";
-        $q6 = "CREATE TABLE posts (postId INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, title TEXT NOT NULL, content TEXT NOT NULL, date TEXT NOT NULL, FOREIGN KEY(author) REFERENCES users(user));";
+        $q6 = "CREATE TABLE posts (postId INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, title TEXT NOT NULL, content TEXT NOT NULL, date TEXT NOT NULL, FOREIGN KEY(author) REFERENCES users(user), INTEGER isAnsweredByDoctor);";
         $q7 = "CREATE TABLE comments(commentId INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT NOT NULL, author TEXT NOT NULL, text INTEGER NOT NULL, belongs_to_post INTEGER NOT NULL, FOREIGN KEY(belongs_to_post) REFERENCES posts(postId));";
         $q8 = "CREATE TABLE payingusers(id INTEGER PRIMARY KEY, banr VARCHAR(16), ispaying INTEGER, totalpayed INTEGER);";
         $q9 = "CREATE TABLE doctors(id INTEGER PRIMARY KEY, totalearned INTEGER);";
@@ -61,8 +61,8 @@ class Sql
     }
 
     static function insertPosts() {
-        $q4 = "INSERT INTO posts(author, date, title, content) VALUES ('bob', '26082015', 'I have a problem', 'I have a generic problem I think its embarrasing to talk about. Someone help?')";
-        $q5 = "INSERT INTO posts(author, date, title, content) VALUES ('bjarni', '26082015', 'I also have a problem', 'I generally fear very much for my health')";
+        $q4 = "INSERT INTO posts(author, date, title, content, isAnsweredByDoctor) VALUES ('bob', '26082015', 'I have a problem', 'I have a generic problem I think its embarrasing to talk about. Someone help?', 0)";
+        $q5 = "INSERT INTO posts(author, date, title, content, isAnsweredByDoctor) VALUES ('bjarni', '26082015', 'I also have a problem', 'I generally fear very much for my health', 0)";
 
         self::$pdo->exec($q4);
         self::$pdo->exec($q5);
